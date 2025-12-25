973eb6e7337394a58133c0442bdb722509f36df8
FDmaster711 <fabiandacal7@gmail.com>
2025-12-25 17:23:02 -0400

    orders fixed3

--- source/validators/order.validator.js
+++ source/validators/order.validator.js
@@ -17,7 +17,7 @@ export const validateCreate = () => [
 
 export const validateStatus = () => [
   param('id').isUUID().withMessage('Invalid order ID format'),
-  body('status').isIn(['PENDING','PAID','SHIPPED','DELIVERED','CANCELLED']).withMessage('Invalid status'),
+  body('newStatus').isIn(['PENDING','PAID','SHIPPED','DELIVERED','CANCELLED']).withMessage('Invalid status'),
   validateFields
 ];
 
