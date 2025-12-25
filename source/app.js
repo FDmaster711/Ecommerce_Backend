488c09b026c1c04aa8f58f77850396bdd55adc5b
FDmaster711 <fabiandacal7@gmail.com>
2025-12-25 16:40:35 -0400

    deploy settings

--- source/app.js
+++ source/app.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { Server } from './Server/server.js';
 
 dotenv.config();
-const port = process.env.API_PORT || 5800;
+const port = process.env.PORT || process.env.API_PORT || 5800;
 
 const server = new Server(port);
 
